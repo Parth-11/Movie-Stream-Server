22306a1a345cc029d27db3b08549371fc9325542
Parth-11 <parth12.mgg@gmail.com>
2025-10-02 20:30:12 +0530

    controller to add movies

--- controllers/movie_controller.go
+++ controllers/movie_controller.go
@@ -8,11 +8,13 @@ import (
 	database "github.com/Parth-11/Movie-Stream-Server/database"
 	model "github.com/Parth-11/Movie-Stream-Server/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -61,5 +63,32 @@ func GetMovieByID() gin.HandlerFunc {
 		}
 
 		ctx.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		c, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie model.Movie
+		if err := ctx.ShouldBindJSON(&movie); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Input"})
+			return
+		}
+
+		if err := validate.Struct(movie); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed", "details": err.Error()})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(c, movie)
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add movie"})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, result)
 	}
 }
