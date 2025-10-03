177e0eebb2458433feee86729b932eda327ca1b7
Parth-11 <parth12.mgg@gmail.com>
2025-10-03 15:21:29 +0530

    set up auth middleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -14,12 +14,25 @@ func AuthMiddleWare() gin.HandlerFunc {
 		if err != nil {
 			ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			ctx.Abort()
+			return
 		}
 
 		if token == "" {
 			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "No token is provided"})
 			ctx.Abort()
+			return
 		}
 
+		claims, err := utils.ValidateToken(token)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("userID", claims.UserID)
+		ctx.Set("role", claims.Role)
+
+		ctx.Next()
 	}
 }
